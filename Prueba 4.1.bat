62fa5046e3a235a82644070c381d15e3aa49609f
JAAP3FUL <134954216+JAAP3FUL@users.noreply.github.com>
2023-05-29 10:07:32 -0600

    Add files via upload

---
+++ Prueba 4.1.bat
@@ -0,0 +1,14 @@
+@cho off
+color 1f
+Title Instalacion de Pureba 4.0
+Goto Start
+:Start
+echo.
+echo.
+echo.
+cls
+echo. Instalando Programa 1
+echo -------------Jaap3ful---------------
+echo.
+echo Instalando Winrar
+winrar-x64-621es /s
